39103b5f60fae1b833362ff08e85136f47b22876
Verclene <xdev58bo@gmail.com>
2017-10-05 19:52:07 +0900

    fix version

--- src/main/java/net/blacklab/lib/EBLib.java
+++ src/main/java/net/blacklab/lib/EBLib.java
@@ -5,6 +5,6 @@ import net.minecraftforge.fml.common.Mod;
 @Mod(modid = "net.blacklab.lib",name="EBLib",version=EBLib.VERSION, acceptedMinecraftVersions="[1.8,1.8.9]")
 public class EBLib {
 
-	public static final String VERSION="3.1.7.10";
+	public static final String VERSION="3.4.7.10";
 	
 }
